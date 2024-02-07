250d82f22b19cbce5ad1a6461b9e6db8024f8956
AlessioPirrotta <alessiopirrotta92@gmail.com>
2024-02-07 18:49:32 +0100

    header component completed

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,51 @@
+<script >
+
+export default{
+  name: 'AppHeader',
+
+data() {
+    return {
+        arrayNav:['character', 'comic', 'movies', 'tv', 'games', 'collectibles', 'videos', 'fans', 'news', 'shop']
+    }
+}
+
+}
+
+
+
+</script>
+
+<template>
+  <div id="main">
+    <div>
+        <img style="scale: .8;" src="../assets/img/dc-logo.png" alt="">
+    </div>
+    <nav>
+        <ul  v-for="(item, index) in arrayNav" :key="index">
+            <li>{{item}}</li>
+        </ul>
+    </nav>
+  </div>
+</template>
+<style lang="scss" scoped >
+
+#main{
+    background-color: white;
+    height: 120px;
+    margin-top: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 200px;
+    
+}
+nav{
+    display: flex;
+}
+ul{
+    list-style: none;
+    text-transform: uppercase;
+    font-weight: 600;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
+import './style/general.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
