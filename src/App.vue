a9ddb19c5b4b2b3c4811c6ac1b9a87964310a8b6
AlessioPirrotta <alessiopirrotta92@gmail.com>
2024-02-08 17:55:25 +0100

    Main component completed

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import AppHeader from './components/AppHeader.vue'
 import AppContents from './components/AppContents.vue'
 import AppShop from './components/AppShop.vue'
+import AppMain from './components/AppMain.vue'
 
 export default{
   components:{
     AppHeader,
     AppContents,
-    AppShop
+    AppShop,
+    AppMain
   }
 }
 </script>
@@ -16,10 +18,9 @@ export default{
   <AppHeader/>
   <AppContents/>
   <AppShop/>
+  <AppMain/>
 </template>
 
 <style lang="scss" >
 @use './style/general.scss' as *;
-
-
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,94 @@
+<script >
+
+export default{
+  name: 'AppMain'
+}
+
+</script>
+<template>
+    <div id="back">
+        <div class="d-flex flex-column">
+            <h2>DC COMICS</h2>
+            <ul>
+                <li>Character</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Videos</li>
+                <li>News</li>
+            </ul>
+            <h2>DC COMICS</h2>
+            <ul>
+                <li>Shop DC</li>
+                <li>Shop DC Collectibles</li>
+                
+                
+            </ul>
+            
+        </div>
+        <div class="ms-4">
+            <h2>DC </h2>
+            <ul>
+                <li>Term</li>
+                <li>Privacy</li>
+                <li>Choices</li>
+                <li>Advertising</li>
+                <li>Jobs</li>
+                <li>Sub</li>
+                <li>Talent</li>
+                <li>CP</li>
+                <li>Ratings</li>
+                <li>Shop</li>
+                <li>Contacts</li>
+            </ul>
+        </div>
+        <div class="ms-4">
+            <h2>DC COMICS</h2>
+            <ul>
+                <li>DC</li>
+                <li>MAD</li>
+                <li>DCk</li>
+                <li>DCu</li>
+                <li>DC pow</li>
+                
+            </ul>
+        </div> 
+        <div id="logo" class="position-absolute">
+            <img class="position-absolute" src="../assets/img/dc-logo-bg.png" alt="">
+        </div>
+    </div>
+    
+   
+</template>
+<style lang="scss" scoped >
+#back{
+    display: flex;
+    background-image: url(../assets/img/footer-bg.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 370px;
+    width: 100.45%;
+    margin: -8px;
+    color: white;
+    padding-left: 300px;
+    padding-top: 50px;
+    position: relative;
+    overflow: hidden;
+    h2{
+        font-size: 20px;
+        font-weight:bold;
+    }
+}
+ul{
+    list-style: none;
+    padding: 0%;
+    color: lightgrey;
+}
+#logo{
+    top: -100px;
+    right: 900px;
+}
+
+
+</style>
